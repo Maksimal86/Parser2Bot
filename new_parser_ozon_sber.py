d953a65f22318c8656dd2835a06b481cc8972a1b
Maksimal <maksimal86@mail.ru>
2023-06-23 23:09:09 +0300

    Test

--- new_parser_ozon_sber.py
+++ new_parser_ozon_sber.py
@@ -4,22 +4,24 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
+import undetected_chromedriver
 import time, datetime, sys, new_get_mass_from_title, lxml, re
 from bs4 import BeautifulSoup
 class Browser:
     def set_options_selenium(self):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ['enable-automation'])
+        #options = webdriver.ChromeOptions()
+        options = undetected_chromedriver.ChromeOptions()
+        #options.add_experimental_option("excludeSwitches", ['enable-automation'])
         options.add_argument("--disable-blink-features")  # отключение функций блинк-рантайм
         options.add_argument("--disable-blink-features=AutomationControlled")
-        #options.add_argument("--headless")  # скрытый запуск браузера
+        options.add_argument("--headless")  # скрытый запуск браузера
         options.add_argument('--no-sandobox')  # режим песочницы
         options.add_argument('--disable-gpu')  # во избежание ошибок
         options.add_argument('--disable-dev-shm-usage')  # для увеличеня памяти для хрома
         # options.add_argument('--disable-brouser-side-navigation')  # прекращение загрузки дополниетльных подресурсов при дляительной загрузки страницы
         options.add_argument('--lang=en')
-        options.add_experimental_option('useAutomationExtension',
-                                        False)  # опция отключает драйвер для установки других расширений Chrome, таких как CaptureScreenshot
+        #options.add_experimental_option('useAutomationExtension',
+        #                                False)  # опция отключает драйвер для установки других расширений Chrome, таких как CaptureScreenshot
         options.add_argument(
             'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.5.715 Yowser/2.5 Safari/537.36')  # меняем заголовок запроса
         prefs = {"profile.managed_default_content_settings.images": 2}  # не загружаем картинки
@@ -28,7 +30,9 @@ class Browser:
     def start_selenium_browser(self, reference):
         self.options = Browser.set_options_selenium(self)
         self.s = Service(executable_path=r'C:\yandexdriver.exe')
-        self.driver = webdriver.Chrome(options=self.options, service=self.s)
+        self.s = Service(executable_path=r'C:\chromedriver.exe')
+        #self.driver = webdriver.Chrome(options=self.options, service=self.s)
+        self.driver = undetected_chromedriver.Chrome(options=self.options, service=self.s)
         self.driver.get(reference)
         page = self.driver.page_source
         with open('index.html', 'w+', encoding='utf-8', errors='ignore') as file:  # запись страницы в файл
@@ -37,6 +41,7 @@ class Browser:
     def stop_selenium(self):
         self.driver.close()
         self.driver.quit()
+        print('stop')
 
     number_of_displayed_price_options = 5
 'https://www.ozon.ru/category/suhie-korma-dlya-koshek-12349/?deny_category_prediction=true&page=2&tf_state=PULKkqCBeYtuvH9FDiq2YTqlB8lHom0Co_Ie8y6MXKKfhcj4'
@@ -44,6 +49,7 @@ class Reference_Ozon(Browser):
     def __init__(self, reference):
         self.reference = reference
         self.driver = Browser.start_selenium_browser(self, reference)
+    #Удалить???
     def get_reference_on_page(self, reference):
         number_of_pages_viewed = 5
         list_of_reference = [reference,]
@@ -57,7 +63,16 @@ class Reference_Ozon(Browser):
             link_to_folowed_page = reference[:match.start()] + f'prediction=true&from_global=true&page={npage}' + '&' + reference[match.end():]
             list_of_reference.append(link_to_folowed_page) # собрали все ссылки на страницы  в список
         print(list_of_reference)
-        return list_of_reference
+        return list_of_reference #удалить№
+    def get_next_page(self):
+        try:
+            try:
+                self.driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div[2]/div[2]/div[2]/div[6]/div[2]/div/div[1]/div[2]/a/div/div').click()
+            except NoSuchElementException:
+                self.driver.find_element(By.XPATH,
+                                         '//*[@id="layoutPage"]/div[1]/div[2]/div[2]/div[2]/div[5]/div[2]/div/div[1]/div[2]/a/div/div').click()
+        except:
+            print('get_next_page', sys.exc_info())
 
     def get_price(self, teg):
         try:
@@ -73,7 +88,7 @@ class Reference_Ozon(Browser):
     def get_reference_on_product(self, teg):
         return self.driver.find_element(By.XPATH,f'//*[@id="paginatorContent"]/div[1]/div/div[{teg}]/div[1]/a').get_attribute("href")
     def get_mass_product_in_kg(self, teg):
-        massa_of_product_in_kg = testre.search_of_mass_product(Reference_Ozon.get_title(self, teg))
+        massa_of_product_in_kg = new_get_mass_from_title.search_of_mass_product(Reference_Ozon.get_title(self, teg))
         if massa_of_product_in_kg:
             return int(massa_of_product_in_kg)
         else:
@@ -95,9 +110,8 @@ class Reference_Ozon(Browser):
             except NoSuchElementException:
                 break
         return price_list_of_products
-    def get_dict_results_for_products(self):  # доделать из-за списка списков
+    def get_dict_results_for_products(self):
         price_list_of_products = Reference_Ozon.get_price_list_of_products(self)
-        dict_price_list_of_piece_goods = {}
         dict_price_list_of_product = {}
         for i in price_list_of_products:
             #print (i)
@@ -108,15 +122,21 @@ class Reference_Ozon(Browser):
             else:
                 dict_price_list_of_product[float(i[1])] = 'руб\n' + str(i[2]) + '\n' + str(i[3])
         return dict_price_list_of_product
-    def sorting_keys_of_dict(self):
-        return sorted(Reference_Ozon.get_dict_results_for_products(self).keys())
-    def get_result_for_bot(self):
+    def add_new_data_to_dict(self, new_big_dict):
+        new_big_dict.update(Reference_Ozon.get_dict_results_for_products(self))
+        return new_big_dict
+    def sorting_keys_of_dict(self, new_big_dict ):
+        #return sorted(Reference_Ozon.get_dict_results_for_products(self).keys())
+        print(sorted(Reference_Ozon.add_new_data_to_dict(self, new_big_dict).keys()))
+        return sorted(Reference_Ozon.add_new_data_to_dict(self, new_big_dict).keys())
+    def get_result_for_bot(self, new_big_dict):
+        print('new_big_dict', new_big_dict)
         n = 0
         result_for_bot = []
-        for key in Reference_Ozon.sorting_keys_of_dict(self):
+        for key in Reference_Ozon.sorting_keys_of_dict(self, new_big_dict):
             n += 1
-            print('№', n, str(key) + '-' + Reference_Ozon.get_dict_results_for_products(self)[key].translate({ord(i): " " for i in "'' "}))
-            result_for_bot.append(('№', n, str(key) + '-' + Reference_Ozon.get_dict_results_for_products(self)[key].translate({ord(i): " " for i in "'' "})))
+            print('№', n, str(key) + '-' + new_big_dict[key].translate({ord(i): " " for i in "'' "}))
+            result_for_bot.append(('№', n, str(key) + '-' + new_big_dict[key].translate({ord(i): " " for i in "'' "})))
             if n == Browser.number_of_displayed_price_options:
                 break
         return result_for_bot
@@ -168,7 +188,7 @@ class Reference_Sber(Browser):
                 discont = '0'
         return discont
     def get_mass_product_in_kg(self, card):
-        return testre.search_of_mass_product(Reference_Sber.get_title(self, card))
+        return new_get_mass_from_title.search_of_mass_product(Reference_Sber.get_title(self, card))
     def result_for_bot(self):
         pass
         #for in Reference_Ozon.
@@ -180,11 +200,20 @@ def main_function_get_product_data(reference):
         received_link=Reference_Sber(reference)
     else:
         return None
-    for i in range(5):
-        received_link.get_price_list_of_products()
-        received_link.get_result_for_bot()
-        received_link.get_reference_on_page(reference)
-        received_link.stop_selenium()
+    number_of_pages_viewed = 5
+    new_big_dict = {}
+    for i in range(number_of_pages_viewed):
+        new_big_dict = received_link.add_new_data_to_dict(new_big_dict)
+        #received_link.get_price_list_of_products()
+
+        #   ############## Продумать последовательный вызов функций
+        received_link.get_next_page()
+    #try:
+        #received_link.sorting_keys_of_dict(new_big_dict)
+        #received_link.stop_selenium()
+    received_link.get_result_for_bot(new_big_dict)
+    #except:
+    #print('main function', sys.exc_info())
 main_function_get_product_data('https://www.ozon.ru/category/suhie-korma-dlya-koshek-12349/?category_was_predicted=true&deny_category_prediction=true&from_global=true&text=корм+для+кошек+сухой&tf')
 #received_link =Reference_Ozon(reference='https://www.ozon.ru/category/suhie-korma-dlya-koshek-12349/probalance-32169982/?deny_category_prediction=true&from_global=true&sorting=ozon_card_price&text=корм+для+кошек+сухой&weight=10000.000%3B36300.000')
 #received_link.set_options_selenium()
